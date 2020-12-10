2c9c57ddebf7d81b53094f6929f2cdd5afdd0862
Nicole Barnett <nicole.barnett.nz@gmail.com>
2020-12-10 23:36:55 +1300

    Added in some imagery to make the page look nicer

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ console.log("app is running");
 $("#go").on("click", (event) => {
     console.log("Go button was clicked!");
     event.preventDefault();
-    const $country = $('input[name="country"]');
-    const $countryTyped = $country.val();
-    const countryData = $.get(`https://restcountries.eu/rest/v2/name/${$countryTyped}`, (data) => {
+    const country = $('input[name="country"]');
+    const countryTyped = country.val();
+    const countryData = $.get(`https://restcountries.eu/rest/v2/name/${countryTyped}`, (data) => {
         const capital = data[0].capital;
         console.log(capital);
         const language = data[0].languages[0].name;
@@ -20,13 +20,17 @@ $("#go").on("click", (event) => {
         const flag = data[0].flag;
         console.log(flag);
         $("#button-one").append(`<img class="images" id="flag-img" src="${flag}"></img>`);
-    });
-
-
-    const countryImages = $.get(`https://api.unsplash.com/search/photos?query=${$countryTyped}&client_id=0GTjLXehAwMxmSfFrV-PDAi35FISuLBpI2zIzpWxZD8`, (data) => {
-        const randomIndex = data.results[Math.floor(Math.random() * data.results.length)];
-        countryImg = randomIndex.urls.small;
-        console.log(countryImg);
-        $("#img-here").append(`<img class="images" id="country-img" src="${countryImg}"></img>`)
+        
+        const capitalImages = $.get(`https://api.unsplash.com/search/photos?query=${capital}&client_id=0GTjLXehAwMxmSfFrV-PDAi35FISuLBpI2zIzpWxZD8`, (data) => {
+            const randomIndex = data.results[Math.floor(Math.random() * data.results.length)];
+            capitalImg = randomIndex.urls.small;
+            $("#button-two").append(`<img class="images" id="capital-img" src="${capitalImg}"></img>`)
         });
+
+        const countryImages = $.get(`https://api.unsplash.com/search/photos?query=${countryTyped}+map&client_id=0GTjLXehAwMxmSfFrV-PDAi35FISuLBpI2zIzpWxZD8`, (data) => {
+            const randomIndex = data.results[Math.floor(Math.random() * data.results.length)];
+            countryImg = randomIndex.urls.small;
+            $("#button-three").append(`<img class="images" id="country-img" src="${countryImg}"></img>`)
+        });
+    });
 });
