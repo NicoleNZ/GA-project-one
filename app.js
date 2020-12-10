c8b0cc8d018d573720828ca00aa7f4a29504218d
Nicole Barnett <nicole.barnett.nz@gmail.com>
2020-12-10 20:24:29 +1300

    Finally got the API to work!

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
+console.log("app is running");
 
-const onButtonClick = (event) => {
+
+
+
+$("#go").on("click", (event) => {
+    console.log("Go button was clicked!");
     event.preventDefault();
     const $country = $('input[name="country"]');
-    $country.val();
-    console.log("val: ", $country.val())
-};
-
-
-$('#button-two').on("click", () => {
-console.log("Capital button clicked");
-const countryData = $.get(`https://restcountries.eu/rest/v2/name/`, (data) => {
-    console.log(data);
-    // countryTyped = data.capital;
-    //const language = data.languages.name; 
-    //const flag = data.flag;
-    //console.log(capital);
-    //console.log(language);
-    //console.timeLog(flag);  
+    const $countryTyped = $country.val();
+    const countryData = $.get(`https://restcountries.eu/rest/v2/name/${$countryTyped}`, (data) => {
+        const capital = data[0].capital;
+        console.log(capital);
+        const language = data[0].languages[0].name;
+        console.log(language);
+        const currencyCode = data[0].currencies[0].code;
+        console.log(currencyCode);
+        const currencyName = data[0].currencies[0].name;
+        console.log(currencyName);
+        const flag = data[0].flag;
+        console.log(flag);
 
     });
+    //$("#result").append(capital);
+});
+
+const countryImages = $.get(`https://api.unsplash.com/photos`, (data) => {
+    const capital = data[0].capital;
+    console.log(capital);
+    const language = data[0].languages[0].name;
+    console.log(language);
+    const currencyCode = data[0].currencies[0].code;
+    console.log(currencyCode);
+    const currencyName = data[0].currencies[0].name;
+    console.log(currencyName);
+    const flag = data[0].flag;
+    console.log(flag);
+
 });
